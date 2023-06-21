6083873fd32d80e295b30dc1fab36248761d14f9
m1 <m1@localhost.localdomain>
2023-06-21 20:41:42 +0200

    initial commit

---
+++ src/test/java/com/example/ticketBookingApp/TicketBookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ticketBookingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketBookingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
